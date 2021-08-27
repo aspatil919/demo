2d89d7429c7753e9514c582a9eb8d0ea0e5f8015
aspatil919 <68045526+aspatil919@users.noreply.github.com>
2021-08-27 14:32:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,43 +1,15 @@
-
-
-
-provider "azurerm" {
-  version = ">=2.0.0"
-    subscription_id = var.var_subscription_id
-    client_id = var.var_client_id
-    tenant_id = var.var_tenant_id
-    client_secret = var.var_client_secret
-   features{}
+resource "azurerm_resource_group" "rg" {
+  name     = "my-resources"
+  location = "West Europe"
 }
 
+module "vnet" {
+  source              = "./modules/vnet"
+  # source              = "Azure/vnet/azurerm"
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+  subnet_prefixes     = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
+  subnet_names        = ["subnet1","subnet2","subnet3"]
 
-terraform {
-    backend "azurerm" {
-        resource_group_name = "demo01"
-        storage_account_name = "demostorage01"
-        container_name = "dev01"
-        key = "mainfile1.tfstate"
-    }
+  depends_on = [azurerm_resource_group.rg]
 }
-
-resource "azurerm_resource_group" "RG" {
-    name     = var.var_RG_name
-    location = var.var_location
-}
-
-resource "azurerm_storage_account" "STG" {
-   name = var.var_Storage_name
-   resource_group_name = azurerm_resource_group.RG.name
-   location = var.var_location
-   account_tier = var.var_account_tier
-   account_replication_type = var.var_account_replication_type
- }
-
-
-//module "storage_account" {
-// source = "./TF/modules"
-// var_location = var.var_location
-//  var_storage_name = var.var_Storage_name
-//  var_account_tier = var.var_account_tier
-//  var_account_replication_type = var.var_account_replication_type  
-//}
